3ae9d4b0e701998e1d415351f396b986510ffae0
sheharyar saeed <sherryjutt932@gmail.com>
2023-06-26 12:40:03 +0500

    while leaving

--- src/app/components/CircleCursor.js
+++ src/app/components/CircleCursor.js
@@ -1,22 +1,74 @@
 import React, { useState, useEffect } from 'react';
 
 const CircleCursor = () => {
-  const [cursorX, setCursorX] = useState(0);
-  const [cursorY, setCursorY] = useState(0);
-  const [targetY, setTargetY] = useState(-40);
-  const [targetX, setTargetX] = useState(500);
+  // const [cursorX, setCursorX] = useState(0);
+  // const [cursorY, setCursorY] = useState(0);
+  // const [targetX, setTargetX] = useState(0);
+  // const [targetY, setTargetY] = useState(0);
+
+  // useEffect(() => {
+  //   const handleMouseMove = (event) => {
+  //     setTargetX(event.clientX);
+  //     setTargetY(event.clientY);
+  //   };
+
+  //   window.addEventListener('mousemove', handleMouseMove);
+
+  //   const updateCursor = () => {
+  //     setCursorX((prevX) => prevX + (targetX - prevX) * 0.6);
+  //     setCursorY((prevY) => prevY + (targetY - prevY) * 0.6);
+  //     requestAnimationFrame(updateCursor);
+  //   };
+
+  //   requestAnimationFrame(updateCursor);
+
+  //   return () => {
+  //     window.removeEventListener('mousemove', handleMouseMove);
+  //   };
+  // }, [targetX, targetY]);
+
+  const [mouseX, setMouseX] = useState(500);
+  const [mouseY, setMouseY] = useState(-50);
+  const [xp, setXp] = useState(500);
+  const [yp, setYp] = useState(-50);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    function handleMouseMove(e) {
+      setMouseX(e.pageX);
+      setMouseY(e.pageY);
+    }
+
+    function handleMouseLeave() {
+      setIsVisible(false);
+    }
+
+    function handleMouseEnter() {
+      setIsVisible(true);
+    }
+
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseleave', handleMouseLeave);
+    document.addEventListener('mouseenter', handleMouseEnter);
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseleave', handleMouseLeave);
+      document.removeEventListener('mouseenter', handleMouseEnter);
+    };
+  }, []);
+
 
   useEffect(() => {
     const handleMouseMove = (event) => {
-      setTargetX(event.clientX);
-      setTargetY(event.clientY);
+      setMouseX(event.clientX);
+      setMouseY(event.clientY);
     };
-
-    window.addEventListener('mousemove', handleMouseMove);
+  window.addEventListener('mousemove', handleMouseMove);
 
     const updateCursor = () => {
-      setCursorX((prevX) => prevX + (targetX - prevX) * 0.6);
-      setCursorY((prevY) => prevY + (targetY - prevY) * 0.6);
+      setXp((prevX) => prevX + (mouseX - prevX) * 0.6);
+      setYp((prevY) => prevY + (mouseY - prevY) * 0.6);
       requestAnimationFrame(updateCursor);
     };
 
@@ -25,7 +77,9 @@ const CircleCursor = () => {
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
-  }, [targetX, targetY]);
+  }, [mouseX, mouseY]);
+
+
 
   return (
     <>
@@ -41,10 +95,25 @@ const CircleCursor = () => {
           mix-blend-mode: difference;
           transform: translate(-50%, -50%);
         }
+        #circle {
+          width: 20px;
+          height: 20px;
+          border-radius: 50px;
+          background-color: #fff;
+          position: fixed;
+          pointer-events: none;
+          z-index: 99999;
+          mix-blend-mode: difference;
+          transform: translate(-50%, -50%);
+        }
       `}</style>
-      <div className="circle" style={{ left: cursorX, top: cursorY }}></div>
+      {/* <div className="circle" style={{ left: cursorX, top: cursorY }}></div> */}
+      {isVisible && (
+        <div id="circle" style={{ left: xp + 'px', top: yp + 'px' }}></div>
+      )}
     </>
   );
 };
 
 export default CircleCursor;
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./page.module.css";
 import "./styles/work.css";
@@ -7,8 +7,15 @@ import React, { useEffect, useState } from "react";
 import { useLayoutEffect, useRef } from "react";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import CircleCursor from './components/CircleCursor';
+import CircleCursor from "./components/CircleCursor";
+import web1 from "./assets/web1.png";
+import web2 from "./assets/web2.png";
+import web3 from "./assets/web3.png";
+import web4 from "./assets/web4.png";
 
+import { LuGithub } from 'react-icons/lu';
+import { TbBrandNextjs } from 'react-icons/tb';
+import { FaReact } from 'react-icons/fa';
 
 gsap.registerPlugin(ScrollTrigger);
 export default function Home() {
@@ -38,23 +45,69 @@ export default function Home() {
   // }, []); // Empty
 
   const main = useRef(null);
-  const work = useRef(null); 
-  const skills = useRef(null); 
-  const edu = useRef(null); 
+  const work = useRef(null);
+  const skills = useRef(null);
+  const edu = useRef(null);
 
-  
+  const [mouseY, setMouseY] = useState(0);
+
   useEffect(() => {
-     
-    gsap.to(work.current, {
+    function handleMouseMove() {
+      setMouseY(window.pageYOffset);
+    }
+
+    document.addEventListener("scroll", handleMouseMove);
+    return () => {
+      document.removeEventListener("scroll", handleMouseMove);
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   const handleMouseMove = (event) => {
+  //     setMouseX(event.clientX);
+  //     setMouseY(event.clientY);
+  //   };
+  // window.addEventListener('mousemove', handleMouseMove);
+
+  //   const updateCursor = () => {
+  //     setXp((prevX) => prevX + (mouseX - prevX) * 0.6);
+  //     setYp((prevY) => prevY + (mouseY - prevY) * 0.6);
+  //     requestAnimationFrame(updateCursor);
+  //   };
+
+  //   requestAnimationFrame(updateCursor);
+
+  //   return () => {
+  //     window.removeEventListener('mousemove', handleMouseMove);
+  //   };
+  // }, [mouseX, mouseY]);
+
+  useEffect(() => {
+    const workList = gsap.utils.toArray(work.current.children);
+
+    gsap.to(workList, {
+      xPercent: -100 * (workList.length - 1),
       scrollTrigger: {
         trigger: work.current,
-        start: "top top",
-        end: "bottom top",
+        // start: "top top",
+        // end: "+=4000",
+        scrub: 1,
+        // snap:1/(workList.length-1),
         pin: true,
-        pinSpacing: false,
-        toggleClass: { targets: work.current, className: "z-index-active" },
-      }
+        end: () => "+=" + work.current.offsetWidth,
+      },
     });
+
+    // gsap.to(work.current, {
+    //   scrollTrigger: {
+    //     trigger: work.current,
+    //     start: "top top",
+    //     end: "bottom top",
+    //     pin: true,
+    //     pinSpacing: false,
+    //     toggleClass: { targets: work.current, className: "z-index-active" },
+    //   }
+    // });
 
     gsap.to(skills.current, {
       scrollTrigger: {
@@ -64,7 +117,7 @@ export default function Home() {
         pin: true,
         pinSpacing: false,
         toggleClass: { targets: skills.current, className: "z-index-active" },
-      }
+      },
     });
 
     gsap.to(edu.current, {
@@ -75,9 +128,8 @@ export default function Home() {
         pin: true,
         pinSpacing: false,
         toggleClass: { targets: edu.current, className: "z-index-active" },
-      }
+      },
     });
-
   }, []); // Empty
 
   // useLayoutEffect(() => {
@@ -91,18 +143,28 @@ export default function Home() {
   //   });
   // }, []);
 
-
   return (
     <>
       <CircleCursor />
-    
+
       <main ref={main} className={styles.main}>
         <section className={styles.hero}>
           {/* <span className={styles.hline} style={{ height: `${vlineHeight}vh` }}></span>
           <span className={styles.vline} style={{ width: `${hlineWidth}vw` }}></span>
           <span className={styles.hline2} style={{ height: `${vlineHeight2}vh` }}></span> */}
 
-          <h1>SHEHARYAR SAEED</h1>
+          <h1
+            style={{ transform: "translateX(" + Math.min(mouseY, 500) + "px)" }}
+          >
+            SHEHARYAR
+          </h1>
+          <h1
+            style={{
+              transform: "translateX(-" + Math.min(mouseY, 500) + "px)",
+            }}
+          >
+            SAEED
+          </h1>
           <marquee className={styles.title} direction="left">
             <span>Web developer</span>
             <span>•</span>
@@ -111,7 +173,118 @@ export default function Home() {
         </section>
 
         <section ref={work} className={styles.work}>
-          <h1>work</h1>
+          <div className={styles.field}>
+            <div className={styles.topic}>
+              <h2>Web developer</h2>
+              <p>
+                Transform Your Online Presence with Simple and Effective Web
+                Development Services
+              </p>
+              <span>
+                <hr/>recents
+              </span>
+            </div>
+            <div className={styles.detail}>
+              <div>
+              <FaReact size={26} />
+                <p>
+                  <b>Single Page</b> <br /> applications in React JS
+                </p>
+              </div>
+
+              <div>
+              <TbBrandNextjs size={26} />
+                <p>
+                  <b>SEO Optimized</b> <br /> web using Next JS
+                </p>
+              </div>
+            </div>
+            <div className={styles.items}>
+              <div className={styles.list}>
+                <div data-imgsha="true" className={styles.item_card}>
+                  <Image src={web2} alt="web portfolio project 1" />
+                  <div className={styles.Card_Det}>
+                    <span>
+                      <ul>
+                        <li>next js</li>
+                        <li>react</li>
+                      </ul>
+                    </span>
+                    <Link href="/">GSA Accounts System</Link>
+                  </div>
+                </div>
+
+                <div data-imgsha="true" className={styles.item_card}>
+                  <Image src={web1} alt="web portfolio project 2" />
+                  <div className={styles.Card_Det}>
+                    <span>
+                      <ul>
+                        <li>react</li>
+                        <li>python</li>
+                        <li>flask</li>
+                      </ul>
+                    </span>
+                    <Link href="/">Final Year Project</Link>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.list}>
+
+                <div data-imgsha="true"  className={`${styles.item_card}`}>
+                <Image src={web3} alt="web portfolio project 3" />
+                  <div className={styles.Card_Det}>
+                  <span>
+                      <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JS</li>
+                        <li>PHP</li>
+                      </ul>
+                    </span>
+                    <Link href="/">Vertiment <br />
+                      E-Commerce</Link>
+                  </div>
+                </div>
+
+                <div data-imgsha="true" className={` ${styles.item_card} ${styles.item_card2}`}>
+                <Image src={web4} alt="web portfolio project 4" />
+                  <div className={styles.Card_Det}>
+                    <Link href="/">Bootstrap</Link>
+                    <p>clone</p>
+                  </div>
+                </div>
+
+                <div
+                  style={{ backgroundColor: "#fff", color: "#000" }}
+                  className={`${styles.item_card} ${styles.item_card2}`}
+                >
+                  <div className={styles.Card_Det}>
+                    <h3>Portfolio</h3>
+                    <p>personal project</p>
+                  </div>
+                </div>
+              <span>
+                <hr />
+                <Link href="/">
+                  <LuGithub size={20} />
+                  <small >Github</small>
+                </Link>
+              </span>
+              </div>
+
+            </div>
+          </div>
+          <div className={styles.field}>
+            <div className={styles.topic}>
+              <h3>Graphic designer</h3>
+              <p>
+                Unleash the Artistry of Graphic Design to Elevate Your Brand's
+                Visual Identity
+              </p>
+            </div>
+            <div className={styles.detail}>Time</div>
+            <div className={styles.items}>Items</div>
+          </div>
         </section>
 
         <section ref={skills} className={styles.skills}>
